af112fe459e95aa427dbb1113e3a6d3390db0466
Victor Gomes <121402298+victor-0x29a@users.noreply.github.com>
2023-07-02 00:08:56 -0400

    add: addon and a fixie
    addon: Event typing
    
    fixie: Bug on return length of users connecteds

--- Server Node.JS/src/Core/Controllers/main.controller.ts
+++ Server Node.JS/src/Core/Controllers/main.controller.ts
@@ -1,12 +1,13 @@
 import {Server, Socket} from 'socket.io';
 import { Room } from '../../types/room.type';
 import ChatModule from '../modules/chat.module'
+import { User } from '../../types/user.type';
 
 class MainController {
 
     private OnConnect(socket: Socket, rooms: Room[], kernel: Server) {
         const indexInRoom = rooms[0].connects.findIndex(User => User.socket.id === socket.id)
-        kernel.to(rooms[0].uuid.toString()).emit("UsersConnecteds", (rooms[0].connects.length + 1).toString());
+        kernel.to(rooms[0].uuid.toString()).emit("UsersConnecteds", (rooms[0].connects.length).toString());
         kernel.to(rooms[0].uuid.toString()).emit("UserConnected", rooms[0].connects[indexInRoom].name + "#" + socket.id.slice(0, 3));
     }
 
@@ -19,10 +20,14 @@ class MainController {
                 socket: socket
             })
 
+
             this.OnConnect(socket, rooms, kernel);
             
 
-            
+            socket.on("Typing", () => {
+                const User: User = rooms[0].connects.find((User)=> User.socket.id === socket.id && User);
+                kernel.to(rooms[0].uuid.toString()).emit("UserTyping", User.name, socket.id);
+            })
 
             socket.on("ChangeNick", (NewNick: string)=> {
                 if (NewNick.length > 25 || !NewNick) {
@@ -43,8 +48,10 @@ class MainController {
                 const indexInRoom = rooms[0].connects.findIndex(User => User.socket.id === socket.id)
                 kernel.to(rooms[0].uuid.toString()).emit("UserDisconnected", rooms[0].connects[indexInRoom].name + "#" + socket.id.slice(0, 3));
                 rooms[0].connects.splice(indexInRoom, 1);
-                kernel.to(rooms[0].uuid.toString()).emit("UsersConnecteds", (rooms[0].connects.length + 1).toString());
+                kernel.to(rooms[0].uuid.toString()).emit("UsersConnecteds", (rooms[0].connects.length).toString());
             })
+
+
         })
     }
 
